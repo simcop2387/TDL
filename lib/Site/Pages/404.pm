3f3ac230d30431c6e5c8e4a89670ec639c9c46d0
Ryan Voots <simcop2387@simcop2387.info>
2011-02-05 11:38:56 -0500

    switched 404 to new api

--- lib/Site/Pages/404.pm
+++ lib/Site/Pages/404.pm
@@ -1,20 +1,14 @@
 package Site::Pages::404;
 use strictures 1;
+use base qw/ Site::Pages /;
 
-use Site::Utils;
+sub handle_GET {
+    my ( $self ) = @_;
 
-sub handle {
-    my ( $req ) = @_;
-    my ( $res, $con, $uri ) = get_request_info( $req );
-
-    if ( $con eq 'GET' ) {
-        $res->status(404);
-        $res->headers({ 'Content-Type' => 'text/html', 'Refresh' => '1; URL=/' });
-        $res->body( '<meta http-equiv="Refresh" content="1; URL=/"> Permanently relocated to /' );
-        return $res;
-    }
-
-    return http_method_not_allowed( $res );
+    $self->res->status(404);
+    $self->res->headers({ 'Content-Type' => 'text/html', 'Refresh' => '1; URL=/' });
+    $self->res->body( '<meta http-equiv="Refresh" content="1; URL=/"> Permanently relocated to /' );
+    return $self->self->res;
 }
 
 1;
