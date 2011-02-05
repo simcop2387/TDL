6d1a40398178f2f7a7fea6b0d4c6d50040751fa8
Ryan Voots <simcop2387@simcop2387.info>
2011-02-05 11:21:54 -0500

    adding copy of Lists, need to change to Todos

---
+++ lib/Site/Pages/AJAJ/Todos/Edit.pm
@@ -0,0 +1,15 @@
+package Site::Pages::AJAJ::Lists::Edit;
+use strictures 1;
+
+use base qw/ Site::Pages /;
+use Site::Utils;
+
+sub handle_POST {
+  my ( $self ) = @_;
+  
+  my $uid = $self->unroll_session();
+  my ($title, $id) = $self->get_params(qw/title id/);
+
+
+
+}
